29ed1393a929d23b9d3beb236dd14369d0b4b990
wendreo <wendreo23@gmail.com>
2025-11-17 09:55:08 -0300

    first commit

---
+++ src/main/java/com/wendreo/br/project_delivery_raizen/ProjectDeliveryRaizenApplication.java
@@ -0,0 +1,13 @@
+package com.wendreo.br.project_delivery_raizen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDeliveryRaizenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDeliveryRaizenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wendreo/br/project_delivery_raizen/ProjectDeliveryRaizenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendreo.br.project_delivery_raizen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDeliveryRaizenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
